3839bb61fc9512dfef275d41705ba1a98e2a3114
Mykyta <nikitya93@gmail.com>
2023-02-05 21:59:33 -0500

    methods_perebora_arr

---
+++ JavaScript_02/JS_ADVANSED/Less_methods_perebora/script.js
@@ -0,0 +1,60 @@
+'use strict';
+
+// filter
+
+const names = ['Ivan', 'Ann', 'Ksenia', 'Valdemart'];
+
+const shortNames = names.filter(function(name) {
+    return name.length < 5;
+});
+
+console.log(shortNames);
+
+
+// map
+/*
+const answers = ['MykytA', 'AnnA', 'Hello'];
+
+const results = answers.map(item => item.toLowerCase());
+
+console.log(results);
+
+*/
+   //  Плохая практика
+let answers = ['MykytA', 'AnnA', 'Hello'];
+
+answers = answers.map(item => item.toLowerCase());
+
+console.log(answers);
+
+
+// every/some
+
+const some = [4, 'sada', 'zcxczsa'];
+
+console.log(some.some(item => typeof(item) === 'number'));
+
+console.log(some.every(item => typeof(item) === 'number'));
+
+
+// reduce
+
+const arr = [4, 5, 1, 3, 2, 6];
+/*                      3    4
+                        4    5
+                        9    1
+                        10   3
+                        13   2
+                        15   6*/
+const res = arr.reduce((sum, current) => sum + current, 3);
+
+console.log(res);
+
+
+/*
+const arr = ['apple', 'pear', 'plam'];
+
+const res = arr.reduce((sum, current) => `${sum}, ${current}`);
+
+console.log(res);
+*/
--- JavaScript_02/JS_ADVANSED/Less_promise/script.js
+++ JavaScript_02/JS_ADVANSED/Less_promise/script.js
@@ -43,6 +43,10 @@ const test = time => {
 //test(1000).then(() => console.log('1000 ms'));
 //test(2000).then(() => console.log('2000 ms'));
 
-Promise.all([test(1000), test(2000)]).then(() =>{
+/*Promise.all([test(1000), test(2000)]).then(() =>{
+    console.log("all");
+});
+*/
+Promise.race([test(1000), test(2000)]).then(() =>{
     console.log("all");
 });
