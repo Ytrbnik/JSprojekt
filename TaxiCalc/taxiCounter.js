4b969c913f66538e23a49f16aaf71e9962ccaa61
Mykyta <nikitya93@gmail.com>
2023-02-26 05:19:11 -0500

    taxiCountre_correction

--- TaxiCalc/taxiCounter.js
+++ TaxiCalc/taxiCounter.js
@@ -1,33 +1,31 @@
 'use strict';
 
-
 fetch('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json')
-    .then(response => response.json())
-    .then((json) => {
-        let usdCourse = json[24].rate;
+.then(response => response.json())
+.then((json) => {
+    let usdCourse = json[24].rate;
 
-        const userImput = prompt('Введіть ціну за таксі черех ","', '');
+    const userImput = prompt('Введіть ціну за таксі черех ","', '');
 
-        const arr = userImput.split(', ');
+    const arr = userImput.split(', ');
 
-        const taxi = arr.map(str => {
-                const num = parseInt(str);
-                return isNaN(num) ? 0 :num;
-        });
-
-        function taxiCounter (taxiValue, usd) {
-            const taxiTotal = taxiValue.reduce((sum, current) => sum + current);
-
-            const usdTotal = Math.round(taxiTotal / usd);
-
-            const showMyTaxi = () => {
-                alert(`Витрачено на таксі ${taxiTotal}грн. або ${usdTotal}доллара.`);
-            };
-
-
-            showMyTaxi();
-        }
-
-        taxiCounter(taxi, usdCourse);
+    const taxi = arr.map(str => {
+        const num = parseInt(str);
+        return isNaN(num) ? 0 :num;
     });
 
+    function taxiCounter (taxiValue, usd) {
+        const taxiTotal = taxiValue.reduce((sum, current) => sum + current);
+
+        const usdTotal = Math.round(taxiTotal / usd);
+
+        const showMyTaxi = () => {
+            alert(`Витрачено на таксі ${taxiTotal} грн. або ${usdTotal} доллара.`);
+        };
+
+
+        showMyTaxi();
+    }
+
+    taxiCounter(taxi, usdCourse);
+});
--- TaxiCalcNoAPI/taxiCounterBasyk.js
+++ TaxiCalcNoAPI/taxiCounterBasyk.js
@@ -1,28 +1,27 @@
 'use strict';
 
-        let usdCourse = 36.57;
+let usdCourse = 36.57;
 
-        const userImput = prompt('Введіть ціну за таксі черех ","', '');
+const userImput = prompt('Введіть ціну за таксі черех ","', '');
 
-        const arr = userImput.split(', ');
+const arr = userImput.split(', ');
 
-        const taxi = arr.map(str => {
-                const num = parseInt(str);
-                return isNaN(num) ? 0 :num;
-        });
+const taxi = arr.map(str => {
+    const num = parseInt(str);
+    return isNaN(num) ? 0 :num;
+});
 
-        function taxiCounter (taxiValue, usd) {
-            const taxiTotal = taxiValue.reduce((sum, current) => sum + current);
+function taxiCounter (taxiValue, usd) {
+    const taxiTotal = taxiValue.reduce((sum, current) => sum + current);
 
-            const usdTotal = taxiTotal / usd;
+    const usdTotal = taxiTotal / usd;
 
-            const showMyTaxi = () => {
-                alert(`Витрачено на таксі ${taxiTotal}грн. або ${usdTotal}доллара.`);
-            };
+    const showMyTaxi = () => {
+        alert(`Витрачено на таксі ${taxiTotal} грн. або ${usdTotal} доллара.`);
+    };
 
 
-            showMyTaxi();
-        }
+    showMyTaxi();
+}
 
-        taxiCounter(taxi, usdCourse);
-
+taxiCounter(taxi, usdCourse);
